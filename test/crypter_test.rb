480e636b50360203de5e5667584b195150df0b20
Altruist2692 <altruist2692@gmail.com>
2015-10-04 17:59:53 +0530

    add test case for decryptor

--- test/crypter_test.rb
+++ test/crypter_test.rb
@@ -8,4 +8,9 @@ class CrypterTest < Minitest::Test
   def test_encrypter
     assert_equal("ojt", CaesarCypher::Encrypt.new("nis", 1).encrypt )
   end
+
+  def test_decryptor
+    encrypted_text = CaesarCypher::Encrypt.new("nis", 1).encrypt
+    assert_equal("nis", CaesarCypher::Decrypt.new(encrypted_text, 1).decrypt)
+  end
 end
